6420f588f1e675422d6349f7893b58149c454ba1
anoca <anatelesitape@gmail.com>
2018-10-23 13:41:29 -0200

    oi

--- visao/produto/carrinho.visao.php
+++ visao/produto/carrinho.visao.php
@@ -3,7 +3,6 @@
 <table class="table">
     <thead>
         <tr>
-            <th>ID</th>
             <th>NOME</th>
             <th>QUANTIDADE</th>
             <th>DELETE</th>
@@ -11,17 +10,23 @@
         </tr>
     </thead>
     <?php foreach ($carrinho as $produto): ?>
-    <tr>
-        <td><?=$produto['idProduto']?></td>
-        <td><?=$produto['nome']?></td>
-        <td><?=$produto['quantidade']?></td>
-        <td><a href="./produto/deletarCarrinho/<?=$produto['idProduto']?>" class="btn btn-danger">del</a></td>
-        <td><?=$produto['preco']?></td>
-    </tr>
+        <tr>
+            <td><?= $produto['nome'] ?></td>
+            <td>
+                <form action="" method="post">
+                    <input type="number" name="quantidade" value="<?= $produto['quantidade'] ?>">
+                    <input type="hidden" name="id" value="<?=$produto["idProduto"] ?>">
+                    <input type="submit" class="btn btn-primary" placeholder="Atualizar quantidade">
+                </form>
+            </td>
+            <td><a href="./produto/deletarCarrinho/<?= $produto['idProduto'] ?>" class="btn btn-danger">del</a></td>
+            <td><?= $produto['preco'] ?></td>
+        </tr>
     <?php endforeach; ?>
 </table>
-
-
-
+<br>
+<?php echo "O valor total da compra é: " . $totalCarrinho; ?>
+<br>
+<br>
 <a href="./produto" class="btn btn-primary">Adicionar novo produto</a>
 
