ec5317b3af0d6c511fecf1ecd580c8d5a7a225be
anoca <42317324+anoca@users.noreply.github.com>
2018-11-16 15:27:01 -0200

    Create cupomControlador.php
    cupomControlador

---
+++ controlador/cupomControlador.php
@@ -0,0 +1,38 @@
+<?php
+
+require "modelo/cupomModelo.php";
+
+/** admin */
+function adicionar() {
+    if (ehPost()) {
+        extract($_POST);
+        alert(adicionarCupom($nomeCupom, $porcentagemDesconto));
+        redirecionar("cupom/index");
+    } else {
+        exibir("cupom/formulario");
+    }
+}
+/** admin */
+function deletar($id) {
+    alert(deletarCupom($id));
+    redirecionar("cupom/index");
+}
+/** admin */
+function editar($id) {
+    if (ehPost()) {
+        $nomeCupom = $_POST["nomeCupom"];
+        $porcentagemDesconto = $_POST["porcentagemDesconto"];
+        alert(editarCupom($id, $nomeCupom, $porcentagemDesconto));
+        redirecionar("cupom/index");
+    } else {
+        $dados['cupom'] = pegarCupomPorId($id);
+        $dados['acao'] = "./cupom/editar/$id";
+        exibir("cupom/formulario", $dados);
+    }
+}
+/** admin */
+function visualizar($id) {
+    $dados['cupom'] = pegarCupomPorId($id);
+    exibir("cupom/visualizar", $dados);
+}
+?>
