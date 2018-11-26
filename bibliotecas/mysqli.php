5343691a88d6a1e22e705fc86cf3895adaae1124
anoca <42317324+anoca@users.noreply.github.com>
2018-11-26 08:40:26 -0200

    Update mysqli.php
    huruu

--- bibliotecas/mysqli.php
+++ bibliotecas/mysqli.php
@@ -1,7 +1,15 @@
 <?php
 
+require_once ("./servicos/manipulacao.php");
 function conn() {
-    $cnx = mysqli_connect("localhost", "root", "", "LOJA");
-    if (!$cnx) die('Deu errado a conexao!');
+	$dados= manipulacao();
+	//print_r ($dados);
+    //$cnx = mysqli_connect("", "", "", "");
+    //if (!$cnx) die('Deu errado a conexao!');
+
+    $cnx= mysqli_connect($dados[0],$dados[1],$dados[2],$dados[3]);
+    if (!$cnx) die ('Deu errado a conexão');
+
+    mysqli_set_charset ($cnx, "UTF8");
     return $cnx;
 }
